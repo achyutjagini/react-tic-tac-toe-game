5a71c585671827360741cd8226eac59da3ce0e68
achyutjagini <achyut.jagini@gmail.com>
2023-02-26 17:44:47 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
-export default function Square() {
-  return <button className="square">X</button>;
+//. The default keyword tells other files using
+//your code that it’s the main function in your file.
+
+function Square() {
+  return <button className="square">1</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
