a94179e452022f334c14d548c625c2f13109137c
achyutjagini <achyut.jagini@gmail.com>
2023-03-03 16:38:11 +0530

    -

--- src/App.js
+++ src/App.js
@@ -56,6 +56,25 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   function handlePlay(nextSquares) {
     // TODO
@@ -63,14 +82,13 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
-  const currentSquares = history[history.length - 1];
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
